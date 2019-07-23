1ee2994fe1da660a48bf186bb27dec2f22e8a536
Christopher <chrisformey@yahoo.com>
2019-07-23 16:46:19 -0500

    add first test case for GET request.

--- test/test-server.js
+++ test/test-server.js
@@ -6,4 +6,23 @@ const expect = chai.expect
 
 chai.use(chaiHttp)
 
-describe('')
+describe('blog-posts', function() {
+    before(function(){
+        return runServer
+    })
+
+
+    after(function(){
+        return closeServer
+    })
+
+
+    it('should return an array of blog post objects' , function(){
+        return chai.request(app)
+        .get('/blog-posts')
+        .then(function(res){
+            expect(res).to.be.an('array')
+            
+        })
+    })
+})
